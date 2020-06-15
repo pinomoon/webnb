25c40385f75eef6b219d8d10c60389867f2cf252
Luca <lucalb10@gmail.com>
2020-06-15 16:32:43 +0200

    base+https

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
         this.state={apiResponse:""};
     }
     callAPI(){
-        fetch("http://localhost:9000/testAPI")
+        fetch("https://localhost:9000/testAPI")
             .then(res=>res.text())
             .then(res=>this.setState({apiResponse:res}))
             .catch(err=>err);
