dab6b50c0367cd52c6d37e0ceb1b88ca71f859d6
Luca <lucalb10@gmail.com>
2020-06-19 11:37:46 +0200

    modifiche-avanzate-servermail

---
+++ api/servermail/configmail.js
@@ -0,0 +1,17 @@
+exports.configmail={
+
+
+    host:'smtp.libero.it',
+        port:587,
+    secure: false,
+    auth:{
+    user: 'webnb-service@libero.it',
+        pass: 'webnb2020'
+},
+    tls:{
+        rejectUnauthorized: false
+    }
+
+
+}
+
