271fc03fcd47a9fcd58b490be77ebff30621cc0d
GianlucaLM-1 <lamalfagianluca12@gmail.com>
2020-07-03 15:37:15 +0200

    ricerca_funzionante

--- api/routes/prenotazione.js
+++ api/routes/prenotazione.js
@@ -17,7 +17,7 @@ async function ricerca(req, res, next) {
     let results = {};
     try {
         await withTransaction(db, async() => {
-
+            let flag;
             if((req.body.luogo==='') || (req.body.luogo===undefined)){
                 req.body.luogo='%';
             }
@@ -28,11 +28,16 @@ async function ricerca(req, res, next) {
                 req.body.tipo='%';
             }
             if(req.body.disdetta_gratuita===undefined || req.body.disdetta_gratuita===''){
-                req.body.disdetta_gratuita='%';
+                req.body.disdetta_gratuita=100000;
+                flag=-1;
             }
-            if(req.body.disdetta_gratuita===1){
-                req.body.disdetta_gratuita='^[0]';
-
+            if(req.body.disdetta_gratuita==='0'){
+                req.body.disdetta_gratuita=1;
+                flag=-1;
+            }
+            if(req.body.disdetta_gratuita==='1'){
+                req.body.disdetta_gratuita=100000;
+                flag=0;
             }
             if(req.body.modalita_di_pagamento==='' || req.body.modalita_di_pagamento===undefined){
                 req.body.modalita_di_pagamento='%';
@@ -50,7 +55,7 @@ async function ricerca(req, res, next) {
                 WHERE struttura.id_struttura=gallery_struttura.id_struttura AND struttura.id_struttura=camera.id_struttura \
                 AND struttura.id_struttura IN ((SELECT struttura.id_struttura FROM struttura , camera WHERE camera.id_struttura=struttura.id_struttura\
                 AND (struttura.nome_struttura LIKE ? OR struttura.regione LIKE ? OR struttura.citta LIKE ? OR struttura.stato LIKE ?) AND camera.numero_posti_letto>=? AND struttura.tipo LIKE ? \
-                AND struttura.disdetta_gratuita LIKE ?  AND struttura.modalita_di_pagamento LIKE ? AND camera.costo_camera<=? AND camera.colazione_inclusa LIKE ? )\
+                AND (struttura.disdetta_gratuita<? AND struttura.disdetta_gratuita>?) AND struttura.modalita_di_pagamento LIKE ? AND camera.costo_camera<=? AND camera.colazione_inclusa LIKE ? )\
                 EXCEPT (SELECT camera.id_struttura FROM camera,prenotazione WHERE prenotazione.id_camera=camera.id_camera \
                 AND (prenotazione.data_fine>? AND prenotazione.data_inizio<?))) GROUP BY struttura.id_struttura, nome_struttura, tipo, indirizzo_struttura, citta, regione, stato, tipo, immagine_1 ORDER BY nome_struttura ASC"
                 , [
@@ -61,6 +66,7 @@ async function ricerca(req, res, next) {
                     req.body.npl,
                     req.body.tipo,
                     req.body.disdetta_gratuita,
+                    flag,
                     req.body.modalita_di_pagamento,
                     req.body.costo_camera,
                     req.body.colazione_inclusa,
