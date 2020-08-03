802c8f40c6088ddc083723b5be0121f04f48d174
GianlucaLM-1 <lamalfagianluca12@gmail.com>
2020-08-03 13:21:59 +0200

    4.0

--- client/src/Component/about_us/AboutUs.jsx
+++ client/src/Component/about_us/AboutUs.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import paperino from '../images/paperino_01.jpg'
-import topolino from '../images/topolino.jpg'
-import spongebob from '../images/sspongebob.jpg'
-import patrick from '../images/patrick.jpg'
+import salvo from '../images/salvo.jpeg'
+import luca from '../images/luca.jpeg'
+import gianluca from '../images/io.jpg'
+import giuseppe from '../images/giuseppe.jpg'
 
 const AboutUs=()=>{
     return(
@@ -11,21 +11,21 @@ const AboutUs=()=>{
             <section class="team-section text-center my-5" style={{height:"auto",width:"100%"}}>
 
                 <h2 class="h1-responsive font-weight-bold my-5">Il nostro team</h2>
-                <p class="grey-text w-responsive mx-auto mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam
-                    eum porro a pariatur veniam.</p>
 
+                <p class="h1-responsive font-weight-bold my-5">Un gruppo di persone che condivide un obiettivo comune può raggiungere l’impossibile.</p>
+                <hr style={{border:"2px solid #ff6300"}}/>
+                <br/>
                 <div class="row" >
 
                     <div class="col-lg-3 col-md-6 mb-lg-0 mb-5" >
                         <div class="avatar mx-auto">
-                            <img src={paperino} width="300" height="300"class="rounded-circle z-depth-1"
+                            <img src={salvo} width="250" height="250"class="rounded-circle z-depth-1"
                                  alt="Sample avatar"/>
                         </div>
                         <h5 class="font-weight-bold mt-4 mb-3">Salvatore Drago</h5>
-                        <p class="text-uppercase blue-text"><strong>Backend Engineer</strong></p>
-                        <p class="grey-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-                            adipisci sed quia non numquam modi tempora eius.</p>
+                        <p class="text-uppercase blue-text"><strong>Backend Developer</strong></p>
+                        <p class="grey-text">Colui che chiede è stupido per un minuto, colui che non chiede è stupido per tutta la vita.  <br/>
+                        -Confucio</p>
                         <ul class="list-unstyled mb-0">
                             <a class="p-2 fa-lg fb-ic">
                                 <i class="fab fa-facebook-f blue-text"> </i>
@@ -41,13 +41,12 @@ const AboutUs=()=>{
 
                     <div class="col-lg-3 col-md-6 mb-lg-0 mb-5">
                         <div class="avatar mx-auto">
-                            <img src={topolino} width="300" height="300" class="rounded-circle z-depth-1"
+                            <img src={luca} width="250" height="250" class="rounded-circle z-depth-1"
                                  alt="Sample avatar"/>
                         </div>
                         <h5 class="font-weight-bold mt-4 mb-3">Luca La Barbera</h5>
-                        <p class="text-uppercase blue-text"><strong>Numero 10 della nazionale</strong></p>
-                        <p class="grey-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem ipsa accusantium
-                            doloremque rem laudantium totam aperiam.</p>
+                        <p class="text-uppercase blue-text"><strong>Backend Developer</strong></p>
+                        <p class="grey-text">Sii il cambiamento che vuoi vedere nel mondo. <br/>  -Mahatma Gandhi</p>
                         <ul class="list-unstyled mb-0">
                             <a class="p-2 fa-lg fb-ic">
                                 <i class="fab fa-facebook-f blue-text"> </i>
@@ -60,13 +59,12 @@ const AboutUs=()=>{
 
                     <div class="col-lg-3 col-md-6 mb-md-0 mb-5">
                         <div class="avatar mx-auto">
-                            <img src={spongebob} width="300" height="300" class="rounded-circle z-depth-1"
+                            <img src={gianluca} width="250" height="250" class="rounded-circle z-depth-1"
                                  alt="Sample avatar"/>
                         </div>
                         <h5 class="font-weight-bold mt-4 mb-3">Gianluca La Malfa</h5>
-                        <p class="text-uppercase blue-text"><strong>Portiere della palazzina</strong></p>
-                        <p class="grey-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim est fugiat nulla id eu laborum.</p>
+                        <p class="text-uppercase blue-text"><strong>Frontend Developer</strong></p>
+                        <p class="grey-text">La perfezione è sempre a un gradino dalla perfezione.</p>
                         <ul class="list-unstyled mb-0">
                             <a class="p-2 fa-lg fb-ic">
                                 <i class="fab fa-facebook-f blue-text"> </i>
@@ -83,13 +81,12 @@ const AboutUs=()=>{
 
                     <div class="col-lg-3 col-md-6">
                         <div class="avatar mx-auto">
-                            <img src={patrick} width="300" height="300" class="rounded-circle z-depth-1"
+                            <img src={giuseppe} width="250" height="250" class="rounded-circle z-depth-1"
                                  alt="Sample avatar"/>
                         </div>
                         <h5 class="font-weight-bold mt-4 mb-3">Giuseppe Luna</h5>
-                        <p class="text-uppercase blue-text"><strong>Frontend developer</strong></p>
-                        <p class="grey-text">Perspiciatis repellendus ad dit consequuntur, eveniet earum nisi qui consectetur
-                            totam officia voluptates perferendis voluptatibus aut.</p>
+                        <p class="text-uppercase blue-text"><strong>Frontend Developer</strong></p>
+                        <p class="grey-text">L’unica vera saggezza è sapere di non sapere nulla. <br/>-Socrate</p>
                         <ul class="list-unstyled mb-0">
 
                             <a class="p-2 fa-lg fb-ic">
